4c3ee112127490df2c1f936436e4e9d094e31582
Sebastian Parra <sebastian20970@gmail.com>
2025-11-23 15:55:32 -0500

    Scripts base del personaje finalizado

--- ProyectoPOO/Assets/Personaje.cs
+++ ProyectoPOO/Assets/Personaje.cs
@@ -1,30 +1,31 @@
 using UnityEngine;
 using UnityEngine.InputSystem; 
+using System.Collections.Generic;
 
 public class Personaje : Entidad
 {
     private int stamina;
-    private int nivel;
-    private int experiencia;
-    private int puntosDeHabilidad;
-    //private Arma arma;
-    // private Habilidad[] habilidades;
+    private int nivel = 0;
+    private int experiencia = 0;
+    private int puntosDeHabilidad = 0;
+    private int monedas = 0;
+    private List<string> llaves = new List<string>();
+    //private List<Arma> armasInventario =new List<Arma>();
+    //private Arma armaActiva;
+    private List<Habilidad> habilidades= new List<Habilidad>();
 
-
-    public Rigidbody2D rb;
-    private Habilidad[] habilidades;
 
     public Rigidbody2D rigidbody2D;
     public float velocidadHorizontal = 1f;
-    public float fuerzaSalto = 4f;
-    public bool espacioPresionado = false;
+     public float fuerzaSalto = 4f;
+    private bool espacioPresionado = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
     void Update()
-    {
+    {   
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             espacioPresionado = true;
@@ -32,6 +33,11 @@ public class Personaje : Entidad
     }
     void FixedUpdate()
     {
+        mover();
+    }
+    public void mover()
+    {
+        
         if (Keyboard.current.dKey.isPressed)
         {
             rb.AddForce(new Vector2(velocidadHorizontal, 0f), ForceMode2D.Impulse);
@@ -54,14 +60,53 @@ public class Personaje : Entidad
             rigidbody2D.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
             espacioPresionado = false;
         }
+
     }
     public override void atacar()
     {
         
     }
-    public override void morir()
+    public void añadirHabilidad(Habilidad habilidad)
     {
+        habilidades.Add(habilidad);
+    }
+    /*public void añadirArmaInventario(Arma arma)
+    {
+        armasInventario.add(arma);
+    }*/
+    /*public void cambiarArmaActiva(Arma arma)
+    {
+        armaActiva = arma;
+    }*/
+    public void aumentarExperiencia(int experiencia)
+    {
+        if (this.experiencia + experiencia >= 100)
+        {
+            nivel++;
+            this.experiencia = this.experiencia + experiencia -100;
+        }
+        else
+        {
+           this.experiencia+= experiencia; 
+        }
         
     }
+    public void añadirMonedas(int monedas)
+    {
+        this.monedas+=monedas;
+    }
+    public int quitarMonedas(int monedas)
+    {
+        if (this.monedas - monedas < 0)
+        {
+            return -1; //no le alcanzaron las monedas
+        }
+        else
+        {
+            this.monedas-=monedas;
+            return 0;
+        }
+    }
+
 }
 
