bbdfb19aed5385500d618887d837dbea1cae2d0d
Sebastian Parra <sebastian20970@gmail.com>
2025-11-24 13:53:05 -0500

    Corrección confusión de archivos
    Habia un archivo llamado personaje y otro llamado jugador, se revisó y se eliminó el que no estaba actualizado

--- ProyectoPOO/Assets/Jugador.cs
+++ ProyectoPOO/Assets/Jugador.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 using UnityEngine.InputSystem; 
 using System.Collections.Generic;
 
-public class Personaje : Entidad
+public class Jugador : Entidad
 {
+    //----------------------------------------------------------Atributos del UML----------------------------------------------------------
     private int stamina;
     private int nivel = 0;
     private int experiencia = 0;
@@ -14,27 +15,35 @@ public class Personaje : Entidad
     //private Arma armaActiva;
     private List<Habilidad> habilidades= new List<Habilidad>();
 
-
-    public Rigidbody2D rigidbody2D;
-    public float velocidadHorizontal = 1f;
-     public float fuerzaSalto = 4f;
+    //----------------------------------------------------------Atributos para funcionamiento en unity----------------------------------------------------------
+    private Rigidbody2D rigidbody2D;
+    public float velocidadHorizontal = 0.2f;
+     public float fuerzaSalto = 5f;
     private bool espacioPresionado = false;
     public float velocidadMaxima = 2f;
     private bool exedioVelMax=false;
     private Vector2 velocidadActual;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform controladorSuelo; 
+    public float radioSuelo = 0.2f;  
+    public LayerMask queEsSuelo;     
+    private bool enSuelo;
+    private Animator animacion;
+
+    //----------------------------------------------------------Metodos de Unity----------------------------------------------------------
     void Start()
     {
-        
+        animacion = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
     void Update()
     {   
+        //Verificaciones para movimiento
         velocidadActual=rigidbody2D.linearVelocity;
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             espacioPresionado = true;
         }
-        if (velocidadActual.x>2f||velocidadActual.x<-2f)
+        if (velocidadActual.x>velocidadMaxima||velocidadActual.x<-velocidadMaxima)
         {
             exedioVelMax=true;
         }
@@ -42,11 +51,38 @@ public class Personaje : Entidad
         {
             exedioVelMax=false;
         }
+
+        //Para animaciones
+        animacion.SetFloat("velocidadX", Mathf.Abs(rigidbody2D.linearVelocity.x));
+        if (rigidbody2D.linearVelocity.x > 0.1f) 
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z); // mirar derecha
+        }
+        else if (rigidbody2D.linearVelocity.x < -0.1f) 
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z); // mirar izquierda
+        }
+        animacion.SetFloat("velocidadY", rigidbody2D.linearVelocity.y);
+        animacion.SetBool("enSuelo",enSuelo);
     }
     void FixedUpdate()
-    {
+    {   
+        //Movimiento en sí
+        enSuelo = Physics2D.OverlapCircle(controladorSuelo.position, radioSuelo, queEsSuelo);
         mover();
     }
+
+    private void OnDrawGizmos()
+    {   
+        //Para dibujar el detector del piso
+        if (controladorSuelo != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(controladorSuelo.position, radioSuelo);
+        }
+    }
+
+    //----------------------------------------------------------Metodos del UML----------------------------------------------------------
     public void mover()
     {
         if (Keyboard.current.dKey.isPressed && !exedioVelMax)
@@ -57,7 +93,11 @@ public class Personaje : Entidad
         {
             rigidbody2D.AddForce(new Vector2(-velocidadHorizontal, 0f), ForceMode2D.Impulse);
         }
-        if (espacioPresionado && !exedioVelMax)
+        if(espacioPresionado && !enSuelo)
+        {
+            espacioPresionado = false;
+        }
+        if (espacioPresionado && enSuelo)
         {
             rigidbody2D.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
             espacioPresionado = false;
@@ -109,6 +149,8 @@ public class Personaje : Entidad
             return 0;
         }
     }
+
+
 
 }
 
